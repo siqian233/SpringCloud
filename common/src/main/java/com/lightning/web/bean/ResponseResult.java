f463e5eff39abb0dbeded742a6888e8e57cfe397
邓皓瑄 <dhc179542022@163.com>
2025-07-11 14:59:43 +0800

    Initial commit

---
+++ common/src/main/java/com/lightning/web/bean/ResponseResult.java
@@ -0,0 +1,35 @@
+package com.lightning.web.bean;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder       //构造器注解，声明后，该类对象不用new来实例化对象，并且可以链式编程。
+public class ResponseResult {
+    private int code;
+    private String msg;
+    private Object data;
+
+    public static ResponseResult ok(){
+        return ResponseResult.builder().code(1).msg("操作成功").build();
+    }
+    public static ResponseResult ok(String msg){
+        return ResponseResult.builder().code(1).msg(msg).build();
+    }
+
+    public static ResponseResult error(){
+        return ResponseResult.builder().code(0).msg("操作失败").build();
+    }
+    public static ResponseResult error(String msg){
+        return ResponseResult.builder().code(0).msg(msg).build();
+    }
+
+    public static ResponseResult noLogin(){
+        return ResponseResult.builder().code(-1).msg("请先登录").build();
+    }
+
+    public <T> ResponseResult setData(T t){
+        this.data = t;
+        return this;
+    }
+}
